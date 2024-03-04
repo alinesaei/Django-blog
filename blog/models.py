3d3a2fae730a29aded153946844ed6d3674abf18
alinesaei <nesaeialii@gmail.com>
2024-03-04 16:17:38 +0330

    Added admin and updated database

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User 
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    # on_delete because if the user created the post and then if the user is delted the post should be deleted
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
-# Create your models here.
+
