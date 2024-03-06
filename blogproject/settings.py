ffa6bf5c6ec56b9ed43bc2625a673ac70f89c8b7
alinesaei <nesaeialii@gmail.com>
2024-03-06 16:20:43 +0330

    added crispy using bootstrap5 for register form

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -28,6 +28,8 @@ from .local_settings import *
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'blog.apps.BlogConfig',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,3 +120,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
